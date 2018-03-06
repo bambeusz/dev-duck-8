a5515ce73adf7cedd774c8fc19105f936c027ef4
Daniel Chmielowiec <daniel.chmielowiec@jazzy.pro>
2018-03-06 12:42:54 +0100

    Sample

--- src/services/ApiResource.ts
+++ src/services/ApiResource.ts
@@ -1,29 +1,95 @@
-interface IResourceBaz {
-    id: string;
-    zar: string[];
+export interface ISensorData {
+    read_at: string;
+    value: number;
+    current_norm?: string;
+    threshold_level: number;
 }
 
-export interface IResource {
-    id: string;
-    foo: string;
-    bar: number[];
-    baz: IResourceBaz;
+export interface INormScope {
+    gte?: number;
+    gt?: number;
+    lte?: number;
+    lt?: number;
 }
 
-const resource: IResource = {
-    id: '321',
-    foo: 'foo',
-    bar: [1, 2, 10, 23, 50],
-    baz: {
-        id: '123',
-        zar: ['Hello', 'Whatever'],
+export interface INorm {
+    [key: string]: INormScope;
+}
+
+export interface ISensor {
+    data: ISensorData[];
+    id: number;
+    name: string;
+    unit: string;
+    norm?: INorm;
+}
+
+export interface IDevice {
+    address: string;
+    city: string;
+    coordinates: number[];
+    id: number;
+    disabled?: boolean;
+    sensors?: ISensor[];
+}
+
+export interface IWidget {
+    device: IDevice;
+    id: number;
+    ordering: number;
+    size: WidgetSize;
+    type: WidgetType;
+    start_date?: string;
+    end_date?: string;
+}
+
+export enum WidgetSize {
+    oneFourth = 1,
+    twoFourths,
+    threeFourths,
+    fourFourths,
+}
+
+export type WidgetType = 'VALUE' | 'CHART' | 'MAP';
+
+const sampleWidget: IWidget = {
+    id: 1,
+    ordering: 44,
+    size: WidgetSize.twoFourths,
+    type: 'VALUE',
+    start_date: '2018-01-05T08:15:30',
+    end_date: '2018-01-09T08:15:30',
+    device: {
+        id: 42,
+        address: 'Gliwice, ul. Mewy',
+        city: 'Gliwice',
+        coordinates: [50.278926, 18.653938],
+        disabled: false,
+        sensors: [
+            {
+                id: 104,
+                data: [
+                    {
+                        current_norm: "grade-c",
+                        read_at: "2018-03-06T10:00:00+01:00",
+                        threshold_level: 143,
+                        value: 71.5166,
+                    }
+                    // ...
+                ],
+                name: 'PM10',
+                unit: 'µg/m³',
+            }
+        ],
     },
 };
 
-class ApiResource {
-    public static getAll(): Promise<IResource[]> {
-        return Promise.resolve([resource]);
-    }
-}
+export default class ApiWidget {
+    // ...
 
-export default ApiResource;
+    public static getWidgets(): Promise<IWidget[]> {
+        return Promise.resolve([sampleWidget])
+    }
+
+    // ...
+}
