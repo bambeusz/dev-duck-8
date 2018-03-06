bb20a71802fe1b0b3e5f25cc37c3958643c28c19
Daniel Chmielowiec <daniel.chmielowiec@jazzy.pro>
2018-03-06 10:45:01 +0100

    Sample

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import Sample from './Sample';
 
 class App extends React.Component<undefined, undefined> {
     public render() {
-        return <Sample baz={3} />;
+        return <Sample />;
     }
 }
 
